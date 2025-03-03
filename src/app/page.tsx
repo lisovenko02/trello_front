474048ef84db99887fc5048dc83232f67786c892
lisovenko02 <vanyalisovenkoo@gmail.com>
2025-03-03 17:36:55 +0200

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import DashboardLayout from '@/components/dashboard-layout/DashboardLayout'
+
 import DashboardPage from './i/page'
 
 export default function Home() {
 	return (
-		<>
+		<DashboardLayout>
 			<DashboardPage />
-		</>
+		</DashboardLayout>
 	)
 }
